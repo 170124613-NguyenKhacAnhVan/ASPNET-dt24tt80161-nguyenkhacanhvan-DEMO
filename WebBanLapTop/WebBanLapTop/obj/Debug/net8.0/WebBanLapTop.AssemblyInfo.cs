7cadfbb00e42f0af1a704e9ff0f80f2d81af9271
170124613-NguyenKhacAnhVan <170124613@rdi.edu.vn>
2026-09-02 13:03:40 +0700

    Thêm Header, Footer cho trang web

--- WebBanLapTop/WebBanLapTop/obj/Debug/net8.0/WebBanLapTop.AssemblyInfo.cs
+++ WebBanLapTop/WebBanLapTop/obj/Debug/net8.0/WebBanLapTop.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebBanLapTop")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5d58e65dbba6e79f159d687a68ca64d06951c87")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebBanLapTop")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebBanLapTop")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
